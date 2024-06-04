d1fc31d5dbf24a4c0a31275d038ba88707859b74
David-11209 <limonchik087@gmail.com>
2024-06-04 15:24:34 +0300

    create project

---
+++ Vulcano/Vulcano/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Vulcano
+//
+//  Created by Давид Васильев on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
